17fd91f036c8bf9ed4ddfa8df1b24d749e0191c8
Sabin Pandey <sabeenpandey2@gmail.com>
2024-11-28 12:24:39 +0545

    aaa

--- backend/src/utils/cookie-methods.utils.ts
+++ backend/src/utils/cookie-methods.utils.ts
@@ -50,9 +50,9 @@ class CookieMethods {
     res.cookie("token", token, {
       maxAge,
       httpOnly: true,
-      sameSite: "lax",
-      domain:
-        process.env.NODE_ENV === "development" ? "localhost" : ".vercel.app",
+      sameSite: "none",
+      // domain:
+      //   process.env.NODE_ENV === "development" ? "localhost" : ".vercel.app",
       // domain: process.env.FRONTEND_URL,
       secure: process.env.NODE_ENV !== "development",
       path: "/",
