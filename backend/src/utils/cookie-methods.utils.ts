ec1fa214b498abf7251d5ec5bf6af51ab80e9fb6
Sabin Pandey <sabeenpandey2@gmail.com>
2024-11-28 10:25:36 +0545

    sasas

--- backend/src/utils/cookie-methods.utils.ts
+++ backend/src/utils/cookie-methods.utils.ts
@@ -50,7 +50,8 @@ class CookieMethods {
     res.cookie("token", token, {
       maxAge,
       httpOnly: true,
-      sameSite: "none",
+      sameSite: "strict",
+      // domain: process.env.FRONTEND_URL,
       secure: process.env.NODE_ENV !== "development",
     });
   }
